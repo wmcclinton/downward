cc06cc220ca0cc1fb78630b4ca9d685e888a55c1
Willie McClinton <wbm3@mit.edu>
2022-09-26 20:48:20 -0400

    pass 85 task

--- behavior_fast-downward.py
+++ behavior_fast-downward.py
@@ -9,9 +9,6 @@ for folder_name in glob.glob(path_to_tasks + "/*"):
     if "domain_" in task_name:
         continue 
     f = open(f"{path_to_tasks}/{task_name}/problem0.pddl", "r")
-    if "stained" in f.read():
-        tasks_ran.append([task_name, False])
-        continue
     f.close()
     # Run Fast Downward
     result = os.popen(f"./fast-downward.py behavior_full.pddl {path_to_tasks}/{task_name}/problem0.pddl --search \"lazy_greedy([ff()], preferred=[ff()])\"").read()
@@ -19,16 +16,22 @@ for folder_name in glob.glob(path_to_tasks + "/*"):
         f = open("sas_plan", "r")
         if "cost = 0" in f.read():
             print(task_name, "fails because of cost = 0 plan")
-            tasks_ran.append([task_name, False])
+            tasks_ran.append([task_name, False, True])
         else:
-            tasks_ran.append([task_name, True])
+            tasks_ran.append([task_name, True, True])
         f.close()
     else:
-        tasks_ran.append([task_name, False])
+        with open(f"{path_to_tasks}/{task_name}/problem0.pddl", "r") as f:
+            pddl_text = f.read()
+        assert "forn" in pddl_text or "forall" in pddl_text or "forpairs" in pddl_text
+        tasks_ran.append([task_name, False, False])
 
 sucesses = [task[0] for task in tasks_ran if task[1]]
-failures = [task[0] for task in tasks_ran if not task[1]]
+failures = [task[0] for task in tasks_ran if not task[1] and task[2]]
+parsing_failures = [task[0] for task in tasks_ran if not task[1] and not task[2]]
 print("Ran on", len(tasks_ran), "tasks")
-print("Failed", len(failures))
+print("Failed", len(failures + parsing_failures))
 print("Success", len(sucesses))
-print(sucesses)
+print("Parsing Failures", len(parsing_failures))
+#print(sucesses)
+print(failures)
